588690b5421546c702d064dadf6f3150e4587e94
Dominic McAdden <dominic.mcadden@gmail.com>
2019-12-03 15:13:57 +0000

    work in progress on angular quiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as data from '../assets/data/questions.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-quiz';
+export class AppComponent implements OnInit {
+  index: number;
+  title: string = 'angular-quiz';
+
+  ngOnInit() {
+    this.index = Math.floor(data.questions.length * Math.random());
+  }
+
+  get question(): any {
+    return data.questions[this.index].question;
+  }
+
+  get answer(): any {
+    return data.questions[this.index].answer;
+  }
 }
